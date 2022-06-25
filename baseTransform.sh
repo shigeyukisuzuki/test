c01e5b98e194f1c97aa621ca1100d5ccdbf06992
Shigeyuki Suzuki <kyuuritosuika@gmail.com>
2022-06-25 15:22:23 +0900

    add option and finalize trap, print result, problem numbering.

--- baseTransform.sh
+++ baseTransform.sh
@@ -1,26 +1,57 @@
 #!/bin/bash
 
+# set default parameter
+numberOfProblem=30
+solvingTime=150
+remainingTime=150
+waitingTime=5
+
+# parse options
+while getopts 'n:l:w:' opt ;do
+	case $opt in
+		n) numberOfProblem=$OPTARG
+			;;
+		l) solvingTime=$OPTARG
+			;;
+		w) waitingTime=$OPTARG
+			;;
+		?) echo "options are -n or -l or -w"
+			;;
+	esac
+done
+
 # make temp file
 temp=$(mktemp)
 
+# finalize
+function finalize {
+	# print answer
+	echo -e "\rvvvvvvvvvvvvvv answer vvvvvvvvvvvvvvvvv"
+	cat $temp | xargs -I@ printf "%4d = 0x%02X\n" @ @ | nl
+
+	# print time lapsed
+	timeLapsed=$(( $solvingTime - $remainingTime ))
+	echo "Result: time Lapsed = $timeLapsed second."
+
+	# delete temp file
+	rm $temp
+}
+
+trap finalize EXIT
+
 # ready
-for i in $(seq 5 -1 1);do
+for i in $(seq $waitingTime -1 1);do
 	echo -n -e "\rready $i  "
 	sleep 1
 done
 
 # print problem
 echo -e "\rvvvvvvvvvvvvvv problem vvvvvvvvvvvvvvvvv"
-shuf -e {1..255} | head -n 20 >> $temp
-cat $temp | xargs -I@ printf "%4d = \n" @ 
-for i in $(seq 100 -1 1);do
+shuf -e {0..255} | head -n $numberOfProblem >> $temp
+cat $temp | xargs -I@ printf "%4d = \n" @ | nl
+for i in $(seq $solvingTime -1 1);do
 	echo -n -e "\r$i  "
+	remainingTime=$i
 	sleep 1
 done
 
-# print answer
-echo -e "\rvvvvvvvvvvvvvv answer vvvvvvvvvvvvvvvvv"
-cat $temp | xargs -I@ printf "%4d = 0x%X\n" @ @ 
-
-# delete temp file
-rm $temp
